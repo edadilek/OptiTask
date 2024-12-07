3cb7227b8ad5a247e95ebf66acb00dd6551db8e1
Mahmut Enes <32mahmutenes32@gmail.com>
2024-12-08 01:47:37 +0300

    Global Hata Denetimi eklendi. Loglama mekanizması eklenecek.

---
+++ OptiTask/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Text.Json;
+using System;
+
+namespace OptiTask.Middlewares
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate requestDelegate, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing the request.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An unexpected error occurred.",
+                Details = ex.Message
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(response);
+
+            return context.Response.WriteAsync(jsonResponse);
+        }
+
+
+
+    }
+}
--- OptiTask/Program.cs
+++ OptiTask/Program.cs
@@ -4,6 +4,7 @@ using StackExchange.Redis;
 using OptiTask.Services;
 using DataAccessLayer.Interface;
 using DataAccessLayer.Repository;
+using OptiTask.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -54,6 +55,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
